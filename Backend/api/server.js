13faf6037df02ca28a2b0cb90248b86ac7ab98ca
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-02 23:34:39 +0530

    added api folder for vercel

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const dbConnect = require("./config/db");
-const userRoute = require("./routes/UserRoutes");
-const AnalyticsRoutes = require("./routes/AnalyticsRoutes");
-const TaskRoutes = require("./routes/TaskRoutes")
-const BoardRoutes = require("./routes/BoardRoutes")
+const dbConnect = require("../config/db");
+const userRoute = require("../routes/UserRoutes");
+const AnalyticsRoutes = require("../routes/AnalyticsRoutes");
+const TaskRoutes = require("../routes/TaskRoutes")
+const BoardRoutes = require("../routes/BoardRoutes")
 const cors = require("cors")
 const PORT = process.env.PORT || 3000
 
