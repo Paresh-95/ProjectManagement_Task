3c89cbba2d0f93fc4be717609cf727b10201720d
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-03 19:41:42 +0530

    paresh

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,41 +1,38 @@
-const express = require("express");
+const express = require("express")
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const dbConnect = require("../config/db");
 const userRoute = require("../routes/UserRoutes");
 const AnalyticsRoutes = require("../routes/AnalyticsRoutes");
-const TaskRoutes = require("../routes/TaskRoutes");
-const BoardRoutes = require("../routes/BoardRoutes");
-const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const TaskRoutes = require("../routes/TaskRoutes")
+const BoardRoutes = require("../routes/BoardRoutes")
+const cors = require("cors")
+const PORT = process.env.PORT || 3000
 
 const app = express();
 dbConnect();
 
-// const corsOptions = {
-//   origin: [
-//     "http://localhost:3000",
-//     `https://project-management-task-u2qo.vercel.app`,
-//   ],
-//   credentials: true,
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
-
-app.use(cors());
-
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://project-management-task-u2qo.vercel.app'],
+    credentials:true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}   
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/v1/auth", userRoute);
-app.use("/api/v1/task", TaskRoutes);
-app.use("/api/v1/board", BoardRoutes);
-app.use("/api/v1/analytics", AnalyticsRoutes);
+app.use("/api/v1/auth",userRoute);
+app.use("/api/v1/task",TaskRoutes)
+app.use("/api/v1/board",BoardRoutes)
+app.use("/api/v1/analytics",AnalyticsRoutes)
 
-app.get("/", (req, res) => {
-  res.send("API HOME ROUTE");
-});
 
-app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT} `);
-});
+
+app.get("/",(req,res)=>{
+    res.send("API HOME ROUTE");
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server Running on ${PORT} `);   
+})
