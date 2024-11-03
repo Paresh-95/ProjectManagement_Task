226708cf388dec30b1a4690e351959675774040d
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-04 01:36:57 +0530

    register

--- Backend/models/User-model.js
+++ Backend/models/User-model.js
@@ -1,36 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  tasks: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Task",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-  boards:[
-    {
-      type:String,
-      unique:true,
-      default:[]
-    }
-  ]
-},{
-  timestamps:true
-});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    tasks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+    boards: {
+      type: [String], // Change this to an array of strings
+      default: [], // Initialize as an empty array
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-module.exports = mongoose.model('User',userSchema)
+module.exports = mongoose.model("User", userSchema);
