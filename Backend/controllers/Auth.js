313f32a6ba9f092aa530d9f25def29abc949c6cd
Paresh-95 <Pareshbhamare95@gmail.com>
2024-11-04 00:08:44 +0530

    secure true

--- Backend/controllers/Auth.js
+++ Backend/controllers/Auth.js
@@ -79,7 +79,7 @@ exports.login = async (req,res) =>{
             const options = {
                 expires: new Date(Date.now() + 3*24*60*60*1000),
                 httpOnly:true,
-                secure:false,
+                secure:true,
                 sameSite:'None'
             }
 
